fdd439a57626ec74e10277938f335fc17a23cd5b
Dor Nudel <dornu23@gmail.com>
2023-01-19 10:25:05 +0200

    Basic health system and damage dealer

--- SpaceShipShooter/Assets/Scripts/EnemySpawner.cs
+++ SpaceShipShooter/Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfigSO> waveConfigs;
     [SerializeField] private float timeBetweenWaves = 0f;
+    [SerializeField] private bool isLooping;
 
     private WaveConfigSO currentWave;
 
@@ -17,20 +18,24 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemyWaves()
     {
-        foreach (WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                GameObject enemy = Instantiate(currentWave.GetEnemyPrefab(i),
-                                               currentWave.GetStartingWaypoint().position,
-                                               Quaternion.identity,
-                                               transform);
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    GameObject enemy = Instantiate(currentWave.GetEnemyPrefab(i),
+                                                   currentWave.GetStartingWaypoint().position,
+                                                   Quaternion.identity,
+                                                   transform);
 
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
         }
+        while (isLooping);
     }
 
     public WaveConfigSO GetCurrentWave()
