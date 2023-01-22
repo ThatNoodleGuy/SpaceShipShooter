0677a3eb5f8712735a8633348b749a130f195b77
Dor Nudel <dornu23@gmail.com>
2023-01-22 10:06:20 +0200

    Parallax Background

---
+++ SpaceShipShooter/Assets/Scripts/SpriteScroller.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScroller : MonoBehaviour
+{
+    [SerializeField] private Vector2 moveSpeed;
+
+    private Vector2 offset;
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        offset = moveSpeed * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+}
